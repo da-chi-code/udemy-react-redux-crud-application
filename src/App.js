83d30a72cd835cfe8d41eac76464e3b18a7c5d40
da-chi-code <tako@takonoMacBook-Air.local>
2020-10-18 15:02:06 +0900

    n Please enter the commit message for your changes. Lines starting

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import React from 'react';
 
-function App() {
+/* function App() {
   return (
     <React.Fragment>
       <label htmlFor="bar">bar</label>
@@ -8,5 +8,31 @@ function App() {
     </React.Fragment >
 
   )
+} */
+const App = () => {
+  const profiles = [{
+    name: "Taro", age: 10
+  }, {
+    name: "Hanako", age: 5
+  },
+  {
+    name: "NoName"
+  }
+
+  ]
+  return (<div>{
+    profiles.map((profiles, index) => {
+      return <User name={profiles.name} age={profiles.age} key={index} />
+    })
+  }
+
+  </div>)
+}
+
+const User = (props) => {
+  return <div>Hi, I am {props.name} ,and {props.age}years old</div>
+}
+User.defaultProps = {
+  age: 1
 }
 export default App;
